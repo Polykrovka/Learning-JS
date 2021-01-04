b316641599471ec1a5263af327d628f94d42f659
Polykrovka <2595092@gmail.com>
2021-01-04 18:22:57 +0300

    Lerning CSS and add file style.css

--- Exercises/exercise15.js
+++ Exercises/exercise15.js
@@ -9,19 +9,45 @@
 
 //------------------------------------------------------------
 
-function sumInput(){
-  let array = [];
-  let sum = 0;
-  let num = 0;
-  do {
-    num = prompt('Введите число ','');
-    array.push(+num);
-  } while(isFinite(num) && num !== '')
-  array.pop();
-  for(let i of array){
-    sum+= + i;
-  }
-  return sum;
-};
+// function sumInput(){
+//   let array = [];
+//   let sum = 0;
+//   let num = 0;
+//   do {
+//     num = prompt('Введите число ','');
+//     array.push(+num);
+//   } while(isFinite(num) && num !== '')
+//   array.pop();
+//   for(let i of array){
+//     sum+= + i;
+//   }
+//   return sum;
+// };
 
-alert(sumInput());
+// alert(sumInput());
+
+
+// -------------------------------------------------------------
+
+function getMaxSubSum(array){
+  let underSum = 0;
+  for(let i of array){
+    if(i < 0){continue}
+    if(i > underSum){
+      underSum += i;
+    }
+  }
+
+
+
+}
+
+
+getMaxSubSum([-1, 2, 3, -9]) //= 5 
+getMaxSubSum([2, -1, 2, 3, -9])// = 6
+getMaxSubSum([-1, 2, 3, -9, 11])// = 11
+getMaxSubSum([-2, -1, 1, 2]) //= 3
+getMaxSubSum([100, -9, 2, -3, 5]) //= 100
+getMaxSubSum([1, 2, 3]) //= 6 (берём все)
+
+getMaxSubSum([-1, -2, -3]) //= 0
